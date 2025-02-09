ff0f09a7b02559f5b89d837a5aaba8d5de0148eb
god <2496538498@qq.com>
2025-02-09 16:12:48 +0800

    modified:   2015/4.c 	new file:   2015/4.exe

--- 2015/4.c
+++ 2015/4.c
@@ -6,8 +6,46 @@
 
 int main() {
     int N;
-    scanf("%d",&N);      //迷宫是N*N的
-//再改点东西看看
+    scanf("%d",&N);      
+    int maze[N][N];    //N*N的迷宫
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            scanf("%d",&maze[i][j]);
+        }
+    }
+    int x,y;   //人所在的横纵坐标
+    scanf("%d%d",&x,&y);
+    int direction;
+    scanf("%d",&direction);
+    //上面全都是输入，俺要开始操作啦
+    if(direction==0){  //左边
+        if(y-1>=0 && maze[x][y-1]==0){
+            printf("Y\n");
+        }else{
+            printf("N\n");
+        }
+    }
+    if(direction==1){  //下边
+        if(x+1<N && maze[x+1][y]==0){
+            printf("Y\n");
+        }else{
+            printf("N\n");
+        }
+    }
+    if(direction==2){  //右边
+        if(y+1<N && maze[x][y+1]==0){
+            printf("Y\n");
+        }else{
+            printf("N\n");
+        }
+    }
+    if(direction==3){  //上边
+        if(x-1>=0 && maze[x-1][y]==0){
+            printf("Y\n");
+        }else{
+            printf("N\n");
+        }
+    }
 
     system("pause");
     return 0;
