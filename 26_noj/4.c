bee0b3e6651ff9861d53301ccd9870f5d4da6447
god <2496538498@qq.com>
2025-12-24 22:11:40 +0800

    modified:   26_noj/.vscode/launch.json 	modified:   26_noj/.vscode/settings.json 	modified:   26_noj/.vscode/tasks.json 	new file:   "26_noj/0_\346\250\241\346\235\277.c" 	new file:   "26_noj/3_\346\207\222\345\276\227\345\206\231.c" 	new file:   26_noj/4.c 	new file:   26_noj/5.c 	new file:   26_noj/test.c 	new file:   26_noj/test.exe

---
+++ 26_noj/4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a,b;
+    scanf("%d %d",&a, &b);
+    printf("%d\n",(a+b)/2);
+
+    system("pause");
+    return 0;
+}
---
+++ 26_noj/test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int n=0;
+	for (int i=1;i<10;i++){
+		if(i<10){
+			n+=i;
+			}
+		}
+		
+		printf("%d\n",n);
+        system("pause");
+		return 0;
+}
